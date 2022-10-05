2796ce4c0c91f3e3e874bc374fba17ba36d32604
Abdullah Chaghtai <halalsenpai@gmail.com>
2022-10-04 22:00:04 -0400

    Added socials

---
+++ config.js
@@ -0,0 +1,20 @@
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+export const socialMediaLinks = [
+  {
+    name: "github",
+    icon: faGithub,
+    link: "https://github.com/halalsenpai",
+  },
+  {
+    name: "linkedin",
+    icon: faLinkedin,
+    link: "https://www.linkedin.com/in/halalsenpai/",
+  },
+  {
+    name: "email",
+    icon: faEnvelope,
+    link: "https://github.com/halalsenpai",
+  },
+];
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "next-themes";
+import { ThemeProvider as MuiThemeProvider, createTheme } from "@mui/material/styles";
+import "../styles/globals.css";
+
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
