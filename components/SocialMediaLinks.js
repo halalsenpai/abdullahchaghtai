74b0e7d587210b0069bfb3e4861e9d0a154b1a9f
Abdullah Chaghtai <halalsenpai@gmail.com>
2022-10-13 21:43:30 -0400

    socials added

---
+++ components/SocialMediaLinks.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { socialMediaLinks } from "../constants/SocialMedia";
+import { Linkedin } from "react-feather";
+
+export const SocialMediaLinks = () => {
+  return (
+    <div className="hidden sm:fixed bottom-0 left-[40px] md:flex flex-col items-center">
+      <div className="flex flex-col">
+        {socialMediaLinks.map((item) => (
+          <a href={item.link} target="_blank" className="hover:-translate-y-1 ease-linear mb-3">
+            {item.icon}
+          </a>
+        ))}
+      </div>
+      <div className="w-[2px] h-[100px] bg-blue-600 dark:bg-yellow-400"></div>
+    </div>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import { Herosection } from "../components/Herosection";
+import { SocialMediaLinks } from "../components/SocialMediaLinks";
 
 export default function Home() {
   return (
     <div>
+      <SocialMediaLinks />
       <main className="dark:bg-[#0a192f]">
         <div className="sm:h-screen sm:flex sm:justify-center sm:items-center sm:min-h-[500px]">
           <Herosection />
